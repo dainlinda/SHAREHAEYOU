be967a9301464eb2718a47ae2f5298d041d7bae7
다인 김 <dainlinda@gmail.com>
2021-03-01 01:02:20 +0900

    app.py port수정

--- app.py
+++ app.py
@@ -316,4 +316,4 @@ class Certificates(Resource):
 api.add_resource(Certificates, '/certificates')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', port=5000,debug=True)
