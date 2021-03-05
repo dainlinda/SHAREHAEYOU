3d79656b34553216356f562706e4c8a69e40aab3
다인 김 <dainlinda@gmail.com>
2021-03-05 17:21:16 +0900

    menu design update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,88 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component, useEffect } from 'react';
 
-//react-router
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import Mine from './components/myportfolio/Mine';
-import Login from './components/user/Login';
-import Signup from './components/user/Signup';
-import Logout from './components/user/Logout';
-import { Vip } from './components/Vip';
-import HistorySample from './components/HistorySample';
-import Menu from './Menu';
+
+import {LoginMenu, LogoutMenu } from './Menu';
 
 function App() {
   const token = localStorage.getItem("token");
   if(token){ //로그인 되어 있을 때 메뉴
     return (
-      <div>
-        <Menu />
-        <BrowserRouter>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/logout">로그아웃</Link>
-            </li>
-            <li>
-              <Link to="/mine">내 포트폴리오 CRUD</Link>
-            </li>
-            <li>
-              <Link to="/vip">vip 접근가능구역</Link>
-            </li>
-            <li>
-              <Link to="/history">예제</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route exact path="/logout" component={Logout}></Route>
-            <Route exact path="/mine" component={Mine}></Route>
-            <Route exact path="/vip" component={Vip}></Route>
-            <Route path="/history" component={HistorySample} />
-          </Switch>
-        </BrowserRouter>
-      </div>
+      <LoginMenu />
     );
   } else { //로그인 안 되어 있을 때 메뉴
     return (
-      <div>
-        <BrowserRouter>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/signup">회원 가입</Link>
-            </li>
-            <li>
-              <Link to="/login">로그인</Link>
-            </li>
-            <li>
-              <Link to="/vip">vip 접근가능구역</Link>
-            </li>
-            <li>
-              <Link to="/history">예제</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route exact path="/signup" component={Signup}></Route>
-            <Route exact path="/login" component={Login}></Route>
-            <Route exact path="/vip" component={Vip}></Route>
-            <Route path="/history" component={HistorySample} />
-          </Switch>
-        </BrowserRouter>
-      </div>
+      <LogoutMenu />
     );
   }
 }
 
-function Home() {
-  return <div>오신 것을 환영합니다! 홈이에요.</div>;
-}
-
 export default App;
-
-
--- react-app/src/Menu.js
+++ react-app/src/Menu.js
@@ -1,22 +1,61 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component, useEffect } from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
-export default function Menu(){
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import Mine from './components/myportfolio/Mine';
+import Logout from './components/user/Logout';
+// import { Vip } from './components/Vip';
+// import HistorySample from './components/HistorySample';
+// import { Home } from './App';
+import Login from './components/user/Login';
+import Signup from './components/user/Signup';
+
+export function LoginMenu(){
     return(
-    <>
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-            {/* <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
-            </Form> */}
-        </Navbar>
-    </>
+        <BrowserRouter>
+            <Navbar bg="dark" variant="dark">
+                <NavLink to="/" className="navbar-brand">Racer Portfolio</NavLink>
+                <Nav className="mr-auto">
+                <NavLink to="/" className="nav-link">Home</NavLink>
+                <NavLink to="/logout" className="nav-link">Logout</NavLink>
+                <NavLink to="/mine" className="nav-link">My portfolio</NavLink>
+                
+                    {/* <Link to="/vip">vip 접근가능구역</Link>
+                    <Link to="/history">예제</Link> */}
+                </Nav>
+            </Navbar>
+            <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/logout" component={Logout}></Route>
+                <Route exact path="/mine" component={Mine}></Route>
+                {/* <Route exact path="/vip" component={Vip}></Route>
+                <Route path="/history" component={HistorySample} /> */}
+            </Switch>
+        </BrowserRouter>
     );
 }
+            
+export function LogoutMenu(){
+    return(
+        <BrowserRouter>
+            <Navbar bg="dark" variant="dark">
+                <NavLink to="/" className="navbar-brand">Racer Portfolio</NavLink>
+                <Nav className="mr-auto">
+                <NavLink to="/" className="nav-link">Home</NavLink>
+                <NavLink to="/signup" className="nav-link">Sign up</NavLink>
+                <NavLink to="/login" className="nav-link">Log in</NavLink>
+                </Nav>
+            </Navbar>
+            <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/signup" component={Signup}></Route>
+                <Route exact path="/login" component={Login}></Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export function Home() {
+    return <div>오신 것을 환영합니다! 홈이에요.</div>;
+  }
